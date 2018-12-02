bc62f3eed6d15b92a89289dfbc2052c88c438df6
longstl <jonnykhanh1102@gmail.com>
2018-12-02 09:49:57 +0700

    Update Reponsitory

---
+++ ASP/Day4/LoginForm/LoginForm/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountContext db = new AccountContext();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View(db.Accounts.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Account account)
+        {
+            var account1 = db.Accounts.Single(acc => acc.username == account.username && acc.password == account.password);
+            if (account1 != null)
+            {
+                return Redirect("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
