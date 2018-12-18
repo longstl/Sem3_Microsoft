ee66b0a7f94cac4d7f9bca7bc5902ab373d36907
longstl <jonnykhanh1102@gmail.com>
2018-12-18 09:30:39 +0700

    Update Reponsitory

---
+++ Web Service/Day2/ConsumingRESTServiceCRUD_Client/ConsumingRESTServiceCRUD_Client/Controllers/BookController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConsumingRESTServiceCRUD_Client.Models;
+
+namespace ConsumingRESTServiceCRUD_Client.Controllers
+{
+    public class BookController : Controller
+    {
+        BookServiceClient bookClient = new BookServiceClient();
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View(bookClient.getAllBooksClient());
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                string createBook = bookClient.AddBook(book);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Book book = bookClient.GetBookById(id);
+            return View(book);
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Book book)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                bookClient.EditBook(book);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Book book = bookClient.GetBookById(id);
+            return View(book);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Book book)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                bookClient.DeleteBook(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Web Service/Day2/ConsumingRESTServiceCRUD_Client/ConsumingRESTServiceCRUD_Client/Models/BookServiceClient.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using ConsumingRESTServiceCRUD_Client.ServiceReference1;
+
+namespace ConsumingRESTServiceCRUD_Client.Models
+{
+    public class BookServiceClient
+    {
+        BookServicesClient client = new BookServicesClient();
+        private readonly string BASE_URL = "http://localhost:2736/BookServices.svc";
+
+        public List<Book> getAllBooksClient()
+        {
+            var list = client.GetBookList().ToList();
+            var rt = new List<Book>();
+            list.ForEach(b => rt.Add(new Book()
+            {
+                BookId = b.BookId,
+                ISBN = b.ISBN,
+                Title = b.Title
+            }));
+            return rt;
+
+            /*var syncClient = new WebClient();
+            var content = syncClient.DownloadString(BASE_URL + "Books");
+            var json_serialize = new JavaScriptSerializer();
+            return json_serialize.Deserialize<List<Book>>(content);*/
+        }
+
+        public string AddBook(Book newBook)
+        {
+            var book = new ServiceReference1.Book()
+            {
+                BookId = newBook.BookId,
+                ISBN = newBook.ISBN,
+                Title = newBook.Title
+            };
+            return client.AddBook(book);
+        }
+
+        public string DeleteBook(int id)
+        {
+            return client.DeleteBook(Convert.ToString(id));
+        }
+
+        public string EditBook(Book newBook)
+        {
+            var book = new ServiceReference1.Book()
+            {
+                BookId = newBook.BookId,
+                ISBN = newBook.ISBN,
+                Title = newBook.Title
+            };
+            return client.UpdateBook(book);
+        }
+
+        public Book GetBookById(int id)
+        {
+            var book = client.GetBookById(Convert.ToString(id));
+            var book1 = new Book()
+            {
+                BookId = book.BookId,
+                ISBN = book.ISBN,
+                Title = book.Title
+            };
+            return book1;
+        }
+    }
+}
